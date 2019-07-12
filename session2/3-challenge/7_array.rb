b5147b37326cdcfdf955a0b41786ad6fc999c64b
dtrts <djt210@gmail.com>
2019-07-12 13:26:44 +0100

    session2. challenge7.

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -2,10 +2,25 @@
 # Punctuation is not part of the word unless it is a contraction.
 # In order to not have to write an actual language parser, there won't be any punctuation too complex.
 # There will be no "'" that is not part of a contraction.
-# Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
+# Assume each of these characters are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
 #
 # Examples
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 
+def alternate_words(sentence)
+  split_list = [" ", "!", "@", "$", "#", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+", "[", "]", ":", ";", ",", ".", "/", "<", ">", "?", "\\", "|"]
+
+  word_array = [sentence]
+
+  split_list.each do |splitter|
+    word_array.collect! do |word|
+      word.split(splitter)
+    end
+    word_array.flatten!
+  end
+
+
+  return word_array.select.with_index {|word,i| i%2==0}
+end
