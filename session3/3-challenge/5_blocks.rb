ccba43be69e7f04cd3fdd46d1727cb8975a77ce2
dtrts <djt210@gmail.com>
2019-07-13 17:17:06 +0100

    session3 challenge5

--- session3/3-challenge/5_blocks.rb
+++ session3/3-challenge/5_blocks.rb
@@ -20,6 +20,50 @@
 # order # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 
 
+def spiral_access(two_d ,&block)
+
+  len = two_d.length
+  return if two_d[0].length <= 0
+  if len == 1
+    block.call(two_d[0][0])
+  elsif len == 2
+    block.call(two_d[0][0])
+    block.call(two_d[0][1])
+    block.call(two_d[1][1])
+    block.call(two_d[1][0])
+  elsif len > 2
+
+    0.upto len - 1 do |i|
+      block.call(two_d[0][i])
+    end
+
+    1.upto len-1 do |i|
+      block.call(two_d[i][len-1])
+    end
+
+    (len-2).downto 0 do |i|
+      block.call(two_d[len-1][i])
+    end
+
+    (len-2).downto 1 do |i|
+      block.call(two_d[i][0])
+    end
+
+    second_two_d = []
+    1.upto (len-2) do |i|
+      second_two_d << two_d[i][1..len-2]
+    end
+
+    spiral_access(second_two_d, &block)
+  else
+    return
+  end
+
+
+
+
+
+end
 
 
 
