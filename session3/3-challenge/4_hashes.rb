6f4e64a1a1c65c32fd091fecd2b0fde8cbf29475
dtrts <djt210@gmail.com>
2019-07-13 16:55:40 +0100

    session3 challenge4

--- session3/3-challenge/4_hashes.rb
+++ session3/3-challenge/4_hashes.rb
@@ -6,3 +6,12 @@
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
 
+def first_pos(string)
+  output = Hash.new
+
+  string.split.each_with_index do |word, index|
+    output[word] = index if output[word] == nil
+  end
+
+  return output
+end
